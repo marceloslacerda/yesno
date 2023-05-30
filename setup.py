eff8f9bbb94d9b42f5da7483251ca85da925d779
Marcelo Lacerda <marceloslacerda@gmail.com>
2023-05-30 20:34:43 -0300

    Remove double punctuation from queries

--- setup.py
+++ setup.py
@@ -9,25 +9,25 @@ from yesno import __version__
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with io.open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with io.open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='yesno',
+    name="yesno",
     version=__version__,
-    description='A module that provides common functions for yes/no CLI queries',
-    author='Marcelo Lacerda',
-    author_email='marceloslacerda@gmail.com',
-    url='https://github.com/marceloslacerda/yesno',
-    packages=['yesno'],
+    description="A module that provides common functions for yes/no CLI queries",
+    author="Marcelo Lacerda",
+    author_email="marceloslacerda@gmail.com",
+    url="https://github.com/marceloslacerda/yesno",
+    packages=["yesno"],
     long_description=long_description,
-    license='BSD',
+    license="BSD",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: CLI :: Utilities',
-        'License :: BSD',
-        'Programming Language :: Python :: 3.4',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: CLI :: Utilities",
+        "License :: BSD",
+        "Programming Language :: Python :: 3.7",
     ],
-      keywords='query cli function',
+    keywords="query cli function",
 )
--- yesno/__init__.py
+++ yesno/__init__.py
@@ -1,18 +1,14 @@
-import sys
-
-if sys.version_info[0] == 2:
-    input = raw_input
-
-__version__ = '0.0.1'
-YES_S = 'yes'
-NO_S = 'no'
+__version__ = "0.1.0"
+YES_S = "yes"
+NO_S = "no"
 YES = True
 NO = False
 INVALID = None
 
+
 def str_to_bool(s):
     """Turns a string to a boolean or None if the supplied string doesn't match
-the value of yes, no, y or n (case insensitive)."""
+    the value of yes, no, y or n (case insensitive)."""
     s = s.strip()
     if (s.lower() == YES_S) | (s.lower() == YES_S[0]):
         return YES
@@ -21,28 +17,31 @@ the value of yes, no, y or n (case insensitive)."""
     else:
         return INVALID
 
-def input_to_bool(query, default='', exit_on_sigint=True, punctuation='?',
-                  return_string=False):
+
+def input_to_bool(
+    query: str, default="", exit_on_sigint=True, punctuation="?", return_string=False
+):
     """Queries the user via stdin/stdout for yes or no, returns the equivalent
-boolean.
+    boolean.
 
 
-    Arguments:
-    query          -- The question to ask the user
-    default        -- The default answer to expect from the user
-                      default('' no default)
-    exit_on_sigint -- Determines whether to exit after a SIGINT or to raise
-                      KeyboardInterruptException
-    punctuation    -- What kind of punctuation to terminate the query with
-    return_string  -- Whether to return the user input with the boolean value.
-"""
+        Arguments:
+        query          -- The question to ask the user
+        default        -- The default answer to expect from the user
+                          default('' no default)
+        exit_on_sigint -- Determines whether to exit after a SIGINT or to raise
+                          KeyboardInterruptException
+        punctuation    -- What kind of punctuation to terminate the query with
+        return_string  -- Whether to return the user input with the boolean value."""
+    if query.endswith(punctuation):
+        punctuation = ""
     try:
         if default:
-            string = input('{}[{}]{} '.format(query, default, punctuation))
+            string = input("{}[{}]{} ".format(query, default, punctuation))
             if not string:
                 string = default
         else:
-            string = input('{}{} '.format(query, punctuation))
+            string = input("{}{} ".format(query, punctuation))
     except KeyboardInterrupt:
         if exit_on_sigint:
             exit(1)
@@ -53,8 +52,10 @@ boolean.
     else:
         return str_to_bool(string)
 
-def input_until_bool(query, error='{} is not one of {}', default='',
-                     exit_on_sigint=True, punctuation='?'):
+
+def input_until_bool(
+    query, error="{} is not one of {}", default="", exit_on_sigint=True, punctuation="?"
+):
     """Works like input_to_bool except it retries if the answer is invalid.
 
 
@@ -65,7 +66,8 @@ def input_until_bool(query, error='{} is not one of {}', default='',
     exit_on_sigint -- Determines whether to exit after a SIGINT or to raise
                       KeyboardInterruptException
     punctuation    -- What kind of punctuation to terminate the query with"""
-
+    if query.endswith(punctuation):
+        punctuation = ""
     while True:
         result = input_to_bool(query, default, exit_on_sigint, punctuation, True)
         if result[1] is not None:
